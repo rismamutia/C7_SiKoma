5d3c0324e7f7755a392b40bc9dacb6cd45af387d
rismamutia <rismamutiadewi89@gmail.com>
2026-04-10 14:20:22 +0700

    membuat tombol button untuk mengubah data

--- CRUDMahasiswaADO/Form1.Designer.cs
+++ CRUDMahasiswaADO/Form1.Designer.cs
@@ -186,7 +186,7 @@
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(75, 23);
             this.button4.TabIndex = 15;
-            this.button4.Text = "button4";
+            this.button4.Text = "Mengubah Data";
             this.button4.UseVisualStyleBackColor = true;
             // 
             // button5
