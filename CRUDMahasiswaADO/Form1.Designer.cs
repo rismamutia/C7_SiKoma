81a40703b57e75a3997056de9de6fe5dd0abe0ed
rismamutia <rismamutiadewi89@gmail.com>
2026-04-10 14:20:52 +0700

    membuat tombol button untuk menghapus data

--- CRUDMahasiswaADO/Form1.Designer.cs
+++ CRUDMahasiswaADO/Form1.Designer.cs
@@ -195,7 +195,7 @@
             this.button5.Name = "button5";
             this.button5.Size = new System.Drawing.Size(75, 23);
             this.button5.TabIndex = 16;
-            this.button5.Text = "button5";
+            this.button5.Text = "Menghapus Data";
             this.button5.UseVisualStyleBackColor = true;
             // 
             // Form1
